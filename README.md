76bf5444d6924f34ad001467cae63c6c04475bbd
Alireza Ahmadi <alireza.elecat@gmail.com>
2019-08-11 18:53:38 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Local-Density-Outlier-Removal-Algorithm
 a small Host/Device implementation of Local-Density  based Outlier Removal Point-Clouds or any Classifiable data-set with distinguishable elements 
+
+
+Local density base: As the sample distribution of
+visual sensors which provide depth information is scattered
+with different densities based on occlusion, viewing angle
+and the environment geometry, the aim of this section is
+to remove inconsistent points from input point set based
+on the geometry and density information. Almost, there are
+three different types of outliers which could be found in
+point clouds, the sparse outlier, isolated outliers comprising
+from a limit number of points grouped together and far
+from true objects region and non-isolated outliers which are
+closer to the model and it’s hard to remove them [15]. If
+we suppose, point set P = {p 1 , p 2 , p 3 , ..., p N }, we need
+to compute the covariance matrix of each point based on
+its neighborhood. Let define query Q = {q 1 , q 2 , q 3 , . . . , q k }
+which shows KN N (pi) as the k Nearest Neighbors of point
+p i . To compute the density information, we need to examine
+each point from point cloud over its k nearest neighbor.
+the local density can be estimated by calculating average
+distance of p i of its k nearest neighbor q j (j = 1, ..., k) as
+
+<div align="center">
+	<img src="/images/ld1.png"  width="300"/>
+</div>
+
+
+where i = 1, 2, ..., k and dist(p i , q j ) is the Euclidian distance
+between p i and q j . Hence the local density can be estimated
+using
+
+<div align="center">
+	<img src="/images/ld2.png"  width="300"/>
+</div>
+
+the implementation of this subject is integrated in the pre-
+processes of the ICP algorithm and the result is depicted in 6.
+As this approach uses KNN radius search over whole points
+in the dataset, is really expensive and without utilizing the
+KD-tree its almost impossible to get proper runtime.
+
+<div align="center">
+	<img src="/images/ld3.png"  width="300"/>
+</div>
+
+Local density Analyses. The out put of outlier removal based on
+local density analyses is depicted in picture in the left beside the original
+dataset which is passed from downsampling step.
---
+++ localDensityOR.hpp
@@ -0,0 +1,147 @@
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Pointcloud Registration C++/ROS Kinect 360-featured base   %
+% by: Alireza Ahmadi                                        %
+% University of Bonn- MSc Robotics & Geodetic Engineering   %
+% Alireza.Ahmadi@uni-bonn.de                                %
+% AlirezaAhmadi.xyz                                         %
+The MIT License
+
+Copyright (c) 2010-2019 Google, Inc. 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+#include <stdlib.h>
+#include <cmath>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <time.h>   
+
+#include <algorithm>
+#include <numeric>
+#include <eigen3/Eigen/Dense>
+
+#include <pcl_ros/point_cloud.h> //pcl
+#include <pcl/point_types.h>
+#include <pcl/common/common_headers.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/point_cloud.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+using namespace std;
+using namespace pcl;
+using namespace cv;
+using namespace Eigen;
+
+#define LD_Search_Type_NN 0         //  0: without FLANNKD-tree (window search in image), 1: with KD-tree 
+#define minDensity 0.2              // minimum local density considered for a single point
+#define knnLD 10                    // number of neighbours to consider for estimateing Density of each point
+#define windowsHeightSize 100       // in pixels (size of search windows)
+
+namespace MapRecon{
+
+    double get_distance(PointXYZRGB& P_new, PointXYZRGB& P_old) {
+        double result = pow((P_new.x - P_old.x),2) + pow((P_new.y - P_old.y),2) + pow((P_new.z - P_old.z), 2);
+        return sqrt(abs(result));
+    }
+
+    void LocalDensityFilter(PointCloud<PointXYZRGB>::Ptr& src,PointCloud<PointXYZRGB>::Ptr& dsr){
+        switch(LD_Search_Type_NN){
+            case 0:{
+                double tempDistance = 100.0;
+                vector<double> Distances;
+                vector<double> minDistances;
+                minDistances.resize(knnLD);
+                int cnt =0;
+                for (int l = 0; l < knnLD; ++l) minDistances[l] = 100;
+
+                pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
+                pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+
+                for (int i=0; i<src->points.size(); i++){
+                    int tmp_loop_min = i - windowsHeightSize;
+                    if(tmp_loop_min < 0) tmp_loop_min = 0; 
+                    int tmp_loop_max = i + windowsHeightSize;
+                    if(tmp_loop_max > src->points.size()) tmp_loop_max = src->points.size(); 
+                    for (int x=tmp_loop_min; x<tmp_loop_max; x++){
+                        if(x != i){
+                            Distances.push_back(get_distance(src->points[i], src->points[x]));
+                            cnt++;
+                            if(knnLD <= cnt){
+                                cnt = 0;
+                                break;
+                            }
+                        }
+                    }
+                    std::sort(Distances.begin(), Distances.end());
+
+                    double d_i = std::accumulate(Distances.begin(), Distances.end(), 0.0)/knnLD;
+                    double target_LD = 0.0;
+                    for (int l = 0; l < knnLD; ++l)
+                    {
+                        target_LD +=exp(-Distances[l]/d_i);
+                    }
+                    target_LD /= K; 
+                    if(minDensity < target_LD){
+                        inliers->indices.push_back(i);
+                    }
+                }
+                extract.setInputCloud(src);
+                extract.setIndices(inliers);
+                extract.setNegative(false);
+                extract.filter(*dsr);
+                break;
+            }case 1:{
+                pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
+                pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+                pcl::KdTreeFLANN<pcl::PointXYZRGB> kdtree;
+                kdtree.setInputCloud (src);
+                for(size_t i = 0; i < src->points.size(); i++){
+                    pcl::PointXYZRGB searchPoint;
+                    searchPoint.x = src->points[i].x;
+                    searchPoint.y = src->points[i].y;
+                    searchPoint.z = src->points[i].z;
+                    std::vector<int> pointIdxNKNSearch(knnLD);
+                    std::vector<float> pointNKNSquaredDistance(knnLD);
+                    int K = kdtree.radiusSearch(searchPoint, this->MaxDist_LD, pointIdxNKNSearch, pointNKNSquaredDistance);
+                    if ( K > 0 )
+                    {
+                        double d_i = std::accumulate(pointNKNSquaredDistance.begin(), pointNKNSquaredDistance.end(), 0.0)/knnLD;
+                        double target_LD = 0.0;
+                        for (int l = 0; l < K; ++l)
+                            target_LD +=exp(-pointNKNSquaredDistance[l]/d_i);
+                        target_LD /= K; 
+                        if(minDensity < target_LD){
+                            inliers->indices.push_back(i);
+                        }
+                    }
+                }
+                extract.setInputCloud(src);
+                extract.setIndices(inliers);
+                extract.setNegative(false);
+                extract.filter(*dsr);
+                break;
+            }
+        }
+    }
+}
+
+
